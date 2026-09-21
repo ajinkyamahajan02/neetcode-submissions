d7aa1545a9608bb1e255a64f7b7e0933e724f663
Ajinkya Narendra Mahajan <71584227+ajinkyamahajan02@users.noreply.github.com>
2026-09-21 12:40:01 +0530

    Add: minimum-window-with-characters - submission-9

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-9.py
@@ -0,0 +1,37 @@
+from collections import Counter
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+
+        def is_subset(subset, superset):
+            for key, count in subset.items():
+                if superset.get(key, 0) < count:
+                    return False
+            return True
+
+        t_map = Counter(t)
+        tempDict = {}
+        l = r = 0
+        winStart = -1
+        winEnd = -1
+        minWin = float("infinity")
+
+        while r < len(s):
+            tempDict[s[r]] = 1 + tempDict.get(s[r], 0)
+            while is_subset(t_map, tempDict):
+                if minWin > r - l + 1:
+                    winStart = l
+                    winEnd = r
+                    minWin = r - l + 1
+                if tempDict.get(s[l]) == 1:
+                    tempDict.pop(s[l])
+                else:
+                    tempDict[s[l]] -= 1
+                l += 1
+            r += 1
+
+        if winEnd == -1 and winStart == -1:
+            return ""
+        return s[winStart:winEnd+1]         
+
+
